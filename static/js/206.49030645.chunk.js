57c031a4923a7a99749ddf099f58ffceb11904f9
VitaliyVovk76 <vovkvitol@gmail.com>
2021-12-28 20:46:54 +0200

    Updates

---
+++ static/js/206.49030645.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook_1=self.webpackChunkgoit_react_hw_08_phonebook_1||[]).push([[206],{1206:function(e,n,o){o.r(n),o.d(n,{default:function(){return r}});var t=o(184);function r(){return(0,t.jsx)("h2",{children:"HomePage"})}}}]);
+//# sourceMappingURL=206.49030645.chunk.js.map
